0e1eeeeb8dabd81f16a2f888355b60056b924552
dar1122 <34757973+dar1122@users.noreply.github.com>
2018-07-19 22:47:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@
 
 
 class cFtp:
-    ftp_ip = '192.168.102.180'
-    ftp_port = 5212
-    ftp_username = 'secu-test'
-    ftp_password = 'secu-test@test'
+    ftp_ip = ''
+    ftp_port = 
+    ftp_username = ''
+    ftp_password = ''
 
 
 
